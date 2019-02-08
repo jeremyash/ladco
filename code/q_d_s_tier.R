edd2bba778494121f36f8ac567d1565bad7036f0
Jeremy Ash <jeremyd.ash@gmail.com>
2019-02-08 15:09:41 -0600

    initial commit

---
+++ code/q_d_s_tier.R
@@ -0,0 +1,26 @@
+## SPATIAL
+library(sp)
+library(rgeos)
+library(raster)
+library(rgdal)
+library(maptools)
+
+## DATA MANAGEMENT
+library(tidyverse)
+library(skimr)
+library(patchwork)
+library(readxl)
+# library(zoo)
+
+## PLOTTING
+library(scales)
+library(units)
+library(viridis)
+library(extrafont)
+library(gtable)
+library(grid)
+#----------------------------------------------------------------------------
+
+
+fac_dat <- read_excel("raw_data/QoverD_V5.6_2016_scc.xlsx",
+                      sheet = "Facility Data")
